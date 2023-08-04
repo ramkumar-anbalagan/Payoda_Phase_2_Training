965821e0ebc66d728d7774b2cc0434edf7002dff
ramkumar-anbalagan <141032760+ramkumar-anbalagan@users.noreply.github.com>
2023-08-04 22:24:18 +0530

    Assignment 2

---
+++ 04.08.2023/Assignment2/Library.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms
+{
+    internal class Library
+    {
+
+        Book[] book = new Book[4];
+        public Library(Book []arr) {
+            book = arr;
+        }
+        public void BorrowBook(string title) {
+            int count = 0;
+            for (int i = 0; i < book.Length; i++) {
+                if (book[i].Title.Equals(title))
+                {
+                    book[i].IsAvailable = false;
+                    Console.WriteLine("Borrowed");
+                    count++;
+                }
+            }
+            if(count == 0) { Console.WriteLine("Book not Available"); }
+        }
+        public void ReturnBook(string title) {
+            for (int i = 0; i < book.Length; i++)
+            {
+                if (book[i].Title.Equals(title))
+                {
+                    book[i].IsAvailable = true;
+                    Console.WriteLine("Returned");
+                }
+            }
+
+        }
+        public void DisplayBookDetails() {
+            for (int i = 0; i < book.Length; i++)
+            {
+                Console.WriteLine("Title :"+book[i].Title+" Author :" + book[i].Author+" Availablity " + book[i].IsAvailable);
+            }
+        }
+    }
+}
