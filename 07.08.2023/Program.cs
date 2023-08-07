19d7f5421d7c0a095f7c2b7c85e7e6f505f48003
ramkumar-anbalagan <141032760+ramkumar-anbalagan@users.noreply.github.com>
2023-08-07 21:31:07 +0530

    Daily Task OOP

---
+++ 07.08.2023/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tasks;
+
+Book book1 = new(101, "The Love", " Manoj", true);
+Book book2 = new(102, "The Thunder", "Muhil", true);
+Book book3 = new(103, "The Avenger", "Ram", false);
+Book book4 = new(104, "Infinity Wars", "Kumar", true);
+
+Library.book.Add(book1);
+Library.book.Add(book2);
+Library.book.Add(book3);
+Library.book.Add(book4);
+
+Library library= new Library();
+int choice = 0;
+while (choice != 4)
+{
+    Console.WriteLine("Choose the option\n1.Borrow Book\n2.Return Book\n3.Display Books\n4.Exit");
+    choice = Convert.ToInt32(Console.ReadLine());
+    if (choice == 1)
+    {
+        Console.WriteLine("Enter the title of the book to borrow");
+        string title = Console.ReadLine();
+        Console.WriteLine(title);
+        library.BorrowBook(title);
+    }
+    else if (choice == 2)
+    {
+        Console.WriteLine("Enter the title of the book to return");
+        string title = Console.ReadLine();
+        library.ReturnBook(title);
+    }
+    else if (choice == 3)
+    {
+        library.DisplayBookDetails();
+    }
+    else if (choice == 4)
+    {
+        break;
+    }
+
+
+}
