b651c4d9ec10360c95100489a1bc72deb2fe10d7
ramkumar-anbalagan <141032760+ramkumar-anbalagan@users.noreply.github.com>
2023-08-20 23:23:10 +0530

    Simple Calculator

---
+++ 19.08.2023/Simple Calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+
+  constructor(){}
+
+  number1: number=0;
+  number2: number=0;
+  operator: string = '+';
+  result: number = 0;
+
+  calculate() {
+    switch (this.operator) {
+      case '+':
+        this.result = this.number1 + this.number2;
+        break;
+      case '-':
+        this.result = this.number1 - this.number2;
+        break;
+      case '*':
+        this.result = this.number1 * this.number2;
+        break;
+      case '/':
+        this.result = this.number1 / this.number2;
+        break;
+      default:
+        this.result = 0;
+    }
+  }
+
+}
